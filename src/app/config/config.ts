bb1b2212e2d16b23a2b01e3568d90e125b35b1f8
Diego Bolanos <blueashym@gmail.com>
2018-07-07 12:03:20 -0500

    Deployment para modelo

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,2 @@
-export const URL_SERVICIOS = 'http://localhost:3000';
+import { environment } from '../../environments/environment';
+export const URL_SERVICIOS =   (environment.production) ? 'https://back-modelo.herokuapp.com' : 'http://localhost:3000';
